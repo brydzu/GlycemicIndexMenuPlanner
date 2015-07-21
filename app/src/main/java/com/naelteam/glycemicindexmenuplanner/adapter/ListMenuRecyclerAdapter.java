36a548d51eb43d734321be22db6d05b39e11e07b
FAB <fabricegen@gmail.com>
2015-07-21 06:59:57 +0100

    update

---
+++ app/src/main/java/com/naelteam/glycemicindexmenuplanner/adapter/ListMenuRecyclerAdapter.java
@@ -0,0 +1,226 @@
+package com.naelteam.glycemicindexmenuplanner.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.naelteam.glycemicindexmenuplanner.R;
+import com.naelteam.glycemicindexmenuplanner.business.GlycemicIndexValueRule;
+import com.naelteam.glycemicindexmenuplanner.model.GlycemicIndex;
+import com.naelteam.glycemicindexmenuplanner.model.GlycemicIndexGroup;
+import com.naelteam.glycemicindexmenuplanner.model.IGlycemicIndex;
+import com.naelteam.glycemicindexmenuplanner.view.GlycemicIndexValueView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListMenuRecyclerAdapter extends RecyclerView.Adapter<ListMenuRecyclerAdapter.MainViewHolder> {
+
+    private final String  TAG = ListMenuRecyclerAdapter.class.getSimpleName();
+
+    private Context mContext;
+    private OnItemClickListener mOnItemClickListener;
+    private List<IGlycemicIndex> mDatas=new ArrayList<IGlycemicIndex>();
+
+    private int mChevronColor;
+
+    public ListMenuRecyclerAdapter(Context context){
+        mContext = context;
+    }
+
+    public ListMenuRecyclerAdapter(Context context, List<IGlycemicIndex> glycemicIndexes, OnItemClickListener clickListener){
+        mContext = context;
+        mOnItemClickListener = clickListener;
+        mDatas.addAll(glycemicIndexes);
+
+        mChevronColor = context.getResources().getColor(android.R.color.holo_blue_light);
+    }
+
+    public void add(IGlycemicIndex glycemicIndex){
+        mDatas.add(glycemicIndex);
+        notifyItemInserted(mDatas.size());
+    }
+
+    @Override
+    public MainViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.list_menu_item, viewGroup, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MainViewHolder viewHolder, int position) {
+
+        if (viewHolder instanceof GroupViewHolder) {
+            final GlycemicIndexGroup glycemicIndexGroup = (GlycemicIndexGroup) mDatas.get(position);
+            final GroupViewHolder groupViewHolder = (GroupViewHolder) viewHolder;
+            groupViewHolder.mTextView.setText(glycemicIndexGroup.getTitle());
+
+            if (glycemicIndexGroup.isExpanded()){
+                groupViewHolder.mImageView.setImageResource(R.drawable.ic_expand_more_black_36dp);
+            }else {
+                groupViewHolder.mImageView.setImageResource(R.drawable.ic_chevron_right_black_36dp);
+            }
+            groupViewHolder.mImageView.setColorFilter(mChevronColor);
+        }else {
+            final GlycemicIndex glycemicIndex = (GlycemicIndex) mDatas.get(position);
+            final ItemViewHolder itemViewHolder = (ItemViewHolder) viewHolder;
+            itemViewHolder.mTextView.setText(glycemicIndex.getTitle());
+            if (glycemicIndex.isSelected()){
+                itemViewHolder.mItemLayout.setBackgroundColor(mContext.getResources().getColor(R.color.blue_light));
+            }else {
+                itemViewHolder.mItemLayout.setBackgroundColor(mContext.getResources().getColor(android.R.color.transparent));
+            }
+            itemViewHolder.mGlycemicIndexValueView.setValue(glycemicIndex.getValue());
+
+            //Log.d(TAG, "onBindViewHolder, position = " + position + ", view = " + itemViewHolder.mGlycemicIndexValueView + ", glycemicIndex title = " + glycemicIndex.getTitle() + ", value = " + glycemicIndex.getValue());
+
+            GlycemicIndexValueRule.setGlycemicIndexValue(mContext, itemViewHolder.mGlycemicIndexValueView, glycemicIndex.getValue());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    public void addAll(List<IGlycemicIndex> glycemicIndexes, int position, int nbItems) {
+        Log.d(TAG, "addAll - position = " + position + ", nbItems = " + nbItems);
+        if (nbItems > 0) {
+            int i = position;
+            for (IGlycemicIndex glycemicIndex : glycemicIndexes) {
+                mDatas.add(i, glycemicIndex);
+                i++;
+            }
+            notifyItemRangeInserted(position, nbItems);
+        }
+    }
+
+    public void removeAll(int position, int nbItems) {
+        Log.d(TAG, "removeAll - position = " + position + ", nbItems = " + nbItems);
+        if (nbItems > 0) {
+            for (int i = position + (nbItems-1); i >= position; i--) {
+                mDatas.remove(i);
+            }
+            notifyItemRangeRemoved(position, (nbItems-1));
+            notifyDataSetChanged();
+        }
+    }
+
+    public void expandGlycemicGroup(GlycemicIndexGroup glycemicIndexGroup, List<IGlycemicIndex> glycemicIndexes, int position){
+        Log.d(TAG, "expandGlycemicGroup - position = " + position);
+
+        glycemicIndexGroup.setExpanded(true);
+        glycemicIndexGroup.setNbItems(glycemicIndexes.size());
+
+        addAll(glycemicIndexes, position + 1, glycemicIndexGroup.getNbItems());
+    }
+
+    public void collapseGlycemicGroup(GlycemicIndexGroup glycemicIndexGroup, int position){
+        removeAll(position + 1, glycemicIndexGroup.getNbItems());
+        glycemicIndexGroup.setExpanded(false);
+        glycemicIndexGroup.setNbItems(0);
+    }
+
+    /**
+     * Select glycemic indexes in the list
+     *
+     * @return int first position of selected item
+     */
+    public int selectItems(List<IGlycemicIndex> glycemicIndexes) {
+        int count = 0;
+        int firstSelectedItemPosition = 0;
+        Log.d(TAG, "selectItems - glycemicIndexes size = " + glycemicIndexes.size());
+        GlycemicIndexGroup dummyGroup = new GlycemicIndexGroup("");
+        for ( IGlycemicIndex glycemicIndex:glycemicIndexes){
+            String title = glycemicIndex.getTitle();
+            dummyGroup.setTitle(title.charAt(0) + "");
+            Log.d(TAG, "selectItems - look for glycemicIndexGroup with title = " + dummyGroup.getTitle());
+            int indexGlycemicGroup = mDatas.indexOf(dummyGroup);
+            GlycemicIndexGroup glycemicIndexGroup = (GlycemicIndexGroup) mDatas.get(indexGlycemicGroup);
+            Log.d(TAG, "selectItems - glycemicIndexGroup.isExpanded() = " + glycemicIndexGroup.isExpanded());
+            if (!glycemicIndexGroup.isExpanded()){
+               mOnItemClickListener.onLoadGIGroup(glycemicIndexGroup, null, indexGlycemicGroup);
+            }
+            int glycemicIndexPosition = mDatas.indexOf(glycemicIndex);
+            if (count==0) firstSelectedItemPosition = glycemicIndexPosition;
+            GlycemicIndex glycemicIndexFromList = (GlycemicIndex) mDatas.get(glycemicIndexPosition);
+            glycemicIndexFromList.setSelected(true);
+
+            count++;
+        }
+        notifyDataSetChanged();
+        return firstSelectedItemPosition;
+    }
+
+    public interface OnItemClickListener {
+        void onLoadGIGroup(GlycemicIndexGroup glycemicIndexGroup, View itemView, int layoutPosition);
+        void onClickGIItem(GlycemicIndex glycemicIndex, int layoutPosition);
+    }
+
+    class MainViewHolder extends RecyclerView.ViewHolder{
+        public MainViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    class GroupViewHolder extends MainViewHolder{
+
+        public TextView mTextView;
+        public ImageView mImageView;
+
+        public GroupViewHolder(final View itemView) {
+            super(itemView);
+
+            mTextView = (TextView) itemView.findViewById(R.id.gi_group_text);
+            mImageView = (ImageView) itemView.findViewById(R.id.gi_group_image);
+            mImageView.setColorFilter(mChevronColor);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, "onLoadGIGroup - view CLICKED");
+
+                    final GlycemicIndexGroup glycemicIndexGroup = (GlycemicIndexGroup) mDatas.get(getLayoutPosition());
+                    if (mOnItemClickListener != null){
+                        mOnItemClickListener.onLoadGIGroup(glycemicIndexGroup, itemView, getLayoutPosition());
+                    }
+                }
+            });
+        }
+    }
+
+    class ItemViewHolder extends MainViewHolder{
+
+        public TextView mTextView;
+        private GlycemicIndexValueView mGlycemicIndexValueView;
+        private LinearLayout mItemLayout;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+
+            mTextView = (TextView) itemView.findViewById(R.id.gi_item_text);
+            mGlycemicIndexValueView = (GlycemicIndexValueView) itemView.findViewById(R.id.gi_value);
+            mItemLayout = (LinearLayout) itemView.findViewById(R.id.gi_item_layout);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, "onClickGIItem - view CLICKED");
+                    final GlycemicIndex glycemicIndex = (GlycemicIndex) mDatas.get(getLayoutPosition());
+
+                    if (mOnItemClickListener != null){
+                        mOnItemClickListener.onClickGIItem(glycemicIndex, getLayoutPosition());
+                    }
+                }
+            });
+
+        }
+    }
+}
---
+++ app/src/main/java/com/naelteam/glycemicindexmenuplanner/fragment/ListMenuFragment.java
@@ -0,0 +1,223 @@
+package com.naelteam.glycemicindexmenuplanner.fragment;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.naelteam.glycemicindexmenuplanner.AppCompatActivityInterface;
+import com.naelteam.glycemicindexmenuplanner.R;
+import com.naelteam.glycemicindexmenuplanner.adapter.ListGIRecyclerAdapter;
+import com.naelteam.glycemicindexmenuplanner.dialog.DialogListener;
+import com.naelteam.glycemicindexmenuplanner.dialog.SearchGIDialog;
+import com.naelteam.glycemicindexmenuplanner.event.UISearchGIEvent;
+import com.naelteam.glycemicindexmenuplanner.model.GlycemicIndex;
+import com.naelteam.glycemicindexmenuplanner.model.GlycemicIndexGroup;
+import com.naelteam.glycemicindexmenuplanner.model.IGlycemicIndex;
+import com.naelteam.glycemicindexmenuplanner.presenter.ListGIPresenter;
+import com.naelteam.glycemicindexmenuplanner.view.DividerItemDecoration;
+import com.naelteam.glycemicindexmenuplanner.view.SlideInRightAnimator;
+
+import java.util.List;
+
+import de.greenrobot.event.EventBus;
+
+
+public class ListMenuFragment extends BaseFragment implements ListGIPresenter.Listener {
+
+    private static final String TAG = ListMenuFragment.class.getSimpleName();
+    private static final String DATA_LIST_KEY = "DATA_LIST_KEY";
+
+    private RecyclerView mRecyclerView;
+    private ListGIPresenter mListGIPresenter;
+    private ListGIRecyclerAdapter mAdapter;
+
+    private ProgressDialog mProgressdialog;
+
+    public ListMenuFragment() {
+    }
+
+    public static Fragment newInstance() {
+        return new ListMenuFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate - savedInstanceState = " + savedInstanceState);
+
+        mListGIPresenter = new ListGIPresenter(this);
+
+        if (savedInstanceState !=null){
+            mListGIPresenter.setDataList(savedInstanceState.getParcelable(DATA_LIST_KEY));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_menu, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Log.d(TAG, "onActivityCreated - ");
+
+        mRecyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        mActivityInterface.showNavigationDrawer();
+
+        mAdapter = new ListGIRecyclerAdapter(getActivity(), mListGIPresenter.getInitialDatas(), new ListGIRecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onLoadGIGroup(GlycemicIndexGroup glycemicIndexGroup, View itemView, int layoutPosition) {
+                Log.d(TAG, "onLoadGIGroup - glycemicIndexGroup.isExpanded() = " + glycemicIndexGroup.isExpanded());
+                if (glycemicIndexGroup.isExpanded()){
+                    mAdapter.collapseGlycemicGroup(glycemicIndexGroup, layoutPosition);
+                }else {
+                    List<IGlycemicIndex> glycemicIndexes = mListGIPresenter.getData(glycemicIndexGroup.getTitle());
+                    if (glycemicIndexes.size() > 0) {
+                        mAdapter.expandGlycemicGroup(glycemicIndexGroup, glycemicIndexes, layoutPosition);
+                        if (itemView != null) {
+                            Snackbar.make(itemView, getString(R.string.loading_data), Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onClickGIItem(GlycemicIndex glycemicIndex, int layoutPosition) {
+                Log.d(TAG, "onClickGIItem - ");
+                mActivityInterface.onDisplayGlycemicIndexDetails(glycemicIndex);
+            }
+        });
+
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
+        SlideInRightAnimator animator = new SlideInRightAnimator();
+        animator.setItemAnimatorFinishedListener(new RecyclerView.ItemAnimator.ItemAnimatorFinishedListener() {
+            @Override
+            public void onAnimationsFinished() {
+                        mAdapter.notifyDataSetChanged();
+            }
+        });
+        mRecyclerView.setItemAnimator(animator);
+        mRecyclerView.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onFloatingActionBarClick(View view) {
+        new SearchGIDialog().createAlertDialog(getActivity(), new DialogListener<String>() {
+            @Override
+            public void onPositiveClick(DialogInterface dialogInterface, String result) {
+                Log.d(getLogTag(), "onPositiveClick - result = " + result);
+                EventBus.getDefault().post(new UISearchGIEvent(result));
+            }
+            @Override
+            public void onNegativeClick(DialogInterface dialogInterface) {
+                dialogInterface.dismiss();
+            }
+        }).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putParcelable(DATA_LIST_KEY, mListGIPresenter.getDataList());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart - ");
+
+        if (mListGIPresenter.getDataCount() == 0) {
+            mRecyclerView.setVisibility(View.GONE);
+            mProgressdialog = ProgressDialog.show(getActivity(), "", "Loading...", true);
+            mListGIPresenter.listGlycementIndexes();
+        }else {
+            mRecyclerView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume - ");
+
+        mListGIPresenter.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause - ");
+
+        mListGIPresenter.pause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop - ");
+    }
+
+    private void closeProgressDialog(){
+        if (mProgressdialog != null){
+            mProgressdialog.cancel();
+        }
+    }
+
+    @Override
+    public void onSearchGI(List<IGlycemicIndex> glycemicIndexes) {
+        int firstPosition = mAdapter.selectItems(glycemicIndexes);
+        mRecyclerView.smoothScrollToPosition(firstPosition);
+    }
+
+    @Override
+    public void onListGISuccess() {
+        closeProgressDialog();
+        Log.d(TAG, "onListGISuccess");
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onListGIError() {
+        closeProgressDialog();
+        Toast.makeText(getActivity(), "Unable to get datas, please try again", Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Log.d(TAG, "onAttach - ");
+
+        try {
+            mActivityInterface = (AppCompatActivityInterface) activity;
+        }catch (ClassCastException e){
+            Log.e(TAG, "onAttach - exception ", e);
+        }
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+}
